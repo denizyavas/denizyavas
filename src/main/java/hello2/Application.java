f3e05123316a1e24d2625f722bdba21dfe667917
Deniz <denizyavas@gmail.com>
2014-12-12 14:37:15 +0000

    heroku config

--- src/main/java/hello2/Application.java
+++ src/main/java/hello2/Application.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 
 }
