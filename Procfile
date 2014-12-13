b616d6268ac4442b8ee602753f8094fec77ccc61
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-12-13 13:44:33 +0200

    heroku deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: java -Dserver.port=$PORT -jar build/libs/denizyavas-1.0.0.jar
+---
+default_process_types:
+  web: java -jar -Dport=$PORT build/libs/denizyavas-all-1.0.0.jar
--- src/main/java/hello2/Application.java
+++ src/main/java/hello2/Application.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 /**
  * author: TRYavasU
  * date: 12/12/2014
@@ -17,6 +19,14 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
 }
