44c6645d84548dda2983bf0914dc4dcbd9208fe3
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-12-21 00:02:26 +0200

    Gradle Build

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 ---
 default_process_types:
-  web: java -jar -Dport=$PORT build/libs/denizyavas-all-1.0.0.jar
+  web: java -jar -Dport=$PORT build/libs/denizyavas-1.0.0.jar
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,4 +1,11 @@
-package hello2;
+package hello;
+
+/**
+ * author: TRYavasU
+ * date: 20/12/2014
+ */
+
+import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,12 +13,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
-/**
- * author: TRYavasU
- * date: 12/12/2014
- */
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
@@ -19,14 +20,6 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
