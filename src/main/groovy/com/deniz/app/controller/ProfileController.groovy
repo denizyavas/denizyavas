1c101f55db8e082c3d34f58167f6a547c1862015
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-12-21 14:53:17 +0200

    Profiles

--- src/main/groovy/com/deniz/app/controller/ProfileController.groovy
+++ src/main/groovy/com/deniz/app/controller/ProfileController.groovy
@@ -1,5 +1,6 @@
 package com.deniz.app.controller
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
@@ -9,10 +10,19 @@ import org.springframework.web.servlet.ModelAndView
  * date: 21/12/2014
  */
 @RestController
-class HelloController {
+class ProfileController {
 
     @RequestMapping("/")
     ModelAndView index() {
         new ModelAndView("redirect:http://goo.gl/CsFh4R");
+    }
+
+    @RequestMapping("/profile/{profile}")
+    ModelAndView profile(@PathVariable("profile") String profile) {
+        if(profile){
+            new ModelAndView("redirect:https://www.facebook.com/denizyavas");
+        }else{
+            new ModelAndView("redirect:http://goo.gl/CsFh4R");
+        }
     }
 }
