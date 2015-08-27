a462ad5076fbe23aa31a74171ee950396cbc4ab4
tryavasu <ugurdeniz.yavas@eu.sony.com>
2015-08-27 23:48:15 +0300

    troubleshoot

--- src/main/java/com/deniz/Application.java
+++ src/main/java/com/deniz/Application.java
@@ -7,6 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("server.port", getHerokuWebPort());
+
         SpringApplication.run(Application.class, args);
+    }
+
+    static String getHerokuWebPort() {
+        String webPort = System.getenv("PORT");
+        if (!webPort.equals("")) {
+            webPort = "8080";
+        }
+        return webPort;
     }
 }
